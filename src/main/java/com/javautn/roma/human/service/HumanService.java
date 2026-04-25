f624300d65796b805f73415d6e0fea4376334d69
--show <samufdev64@gmail.com>
2026-04-25 20:24:26 -0300

    HumanService implemented

--- src/main/java/com/javautn/roma/human/service/HumanService.java
+++ src/main/java/com/javautn/roma/human/service/HumanService.java
@@ -2,6 +2,7 @@ package com.javautn.roma.human.service;
 
 import com.javautn.roma.human.entity.CitizenEntity;
 import com.javautn.roma.human.entity.SlaveEntity;
+import com.javautn.roma.human.repository.HumanRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +10,12 @@ import java.util.List;
 
 @Service
 public class HumanService {
+
+    final HumanRepository humanRepo;
+
+    public HumanService(final HumanRepository humanRepo) {
+        this.humanRepo = humanRepo;
+    }
 
     public List<CitizenEntity> getAllCitizen() {
         return new ArrayList<CitizenEntity>();
@@ -19,34 +26,44 @@ public class HumanService {
     }
 
     public CitizenEntity getCitizen(final long id) {
-        return null;
+        return (CitizenEntity) humanRepo.getReferenceById(id);
     }
 
     public SlaveEntity getSlave(final long id) {
-        return null;
+        return (SlaveEntity) humanRepo.getReferenceById(id);
     }
 
     public CitizenEntity createCitizen(CitizenEntity citizen) {
-        return citizen;
+        final CitizenEntity newCitizen = humanRepo.save(citizen);
+        humanRepo.flush();
+        return newCitizen;
     }
 
     public SlaveEntity createSlave(SlaveEntity slave) {
-        return slave;
+        final SlaveEntity newSlave = humanRepo.save(slave);
+        humanRepo.flush();
+        return newSlave;
     }
 
     public CitizenEntity updateCitizen(final long id, final CitizenEntity citizen) {
-        return null;
+        if (!humanRepo.existsById(id)) return null;
+        citizen.setId(id);
+        return humanRepo.saveAndFlush(citizen);
     }
 
     public SlaveEntity updateSlave(final long id, final SlaveEntity slave) {
-        return null;
+        if (!humanRepo.existsById(id)) return null;
+        slave.setId(id);
+        return humanRepo.saveAndFlush(slave);
     }
 
     public void deleteCitizen(final long id) {
-
+        humanRepo.deleteById(id);
+        humanRepo.flush();
     }
 
     public void deleteSlave(final long id) {
-
+        humanRepo.deleteById(id);
+        humanRepo.flush();
     }
 }
